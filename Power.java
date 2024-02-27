21184330c98d7db68200d4188421b5c4a3c47eaf
HazelYe2 <Hazel.Ye25@ycdsbk12.ca>
2024-02-26 22:02:33 -0500

    Power.java

--- Power.java
+++ Power.java
@@ -1,13 +1,22 @@
 class Power extends ConsoleProgram {
 
   /**
-  * Description
-  * @author:
+  * Print out a to the power of b
+  * @author: H. Ye
   */
   
   public void run() {
     
-    // start coding here
+    // Assign variables
+    double dblA;
+    double dblB;
     
+    // Assign values
+    dblA = readDouble ("Enter the value of A: ");
+    dblB = readDouble ("Enter the value of B: ");
+
+    // Print out
+    System.out.println(Math.pow (dblA, dblB));
+
   }
 }
