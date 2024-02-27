3d0c94601b662c1fcbf845f40b2f5dc09d7e6e01
HazelYe2 <Hazel.Ye25@ycdsbk12.ca>
2024-02-26 22:03:08 -0500

    Minutes.java

--- Minutes.java
+++ Minutes.java
@@ -1,13 +1,29 @@
 class Minutes extends ConsoleProgram {
 
   /**
-  * Description
-  * @author:
+  * Convert minutes to days, hours, and minutes
+  * @author: H. Ye
   */
   
   public void run() {
     
-    // start coding here
+    // Assign variables
+    int intDays;
+    int intHours;
+    int intMin;
+    int intMin2;
+
+    // Assign values
+    intMin = readInt ("Enter the number of minutes: ");
+
+    // Calculate the days, hours, and minutes
+    intDays = intMin/1440;
+    intMin2 = intMin % 1440;
+    intHours = intMin2/60;
+    intMin2 = intMin2 % 60;
+
+    // Print out the days, hours, and minutes
+    System.out.println ("The result of the conversion is: " + intDays + intHours + intMin2);
     
   }
 }
