11039a18ebb7f6efcb3e33ae2a0244d3c758f7ae
HazelYe2 <Hazel.Ye25@ycdsbk12.ca>
2024-02-26 20:25:39 -0500

    FtoC.java

--- FtoC.java
+++ FtoC.java
@@ -1,13 +1,24 @@
 class FtoC extends ConsoleProgram {
 
   /**
-  * Description
-  * @author:
+  * Convert fahrenheit to celcius degree measure
+  * @author: H. Ye
   */
   
   public void run() {
     
-    // start coding here
+    // Assign variables
+    double dblFahr;
+    double dblCel;
+
+    // Assign values
+    dblFahr = readDouble ("Enter the temperature in degrees fahrenheit: ");
+
+    // Calculate the celcius
+    dblCel = ((5/9) * (dblFahr - 32));
+
+    // Print out the celcius 
+    System.out.println ("The degrees in celcius is: " + dblCel);
     
   }
 }
