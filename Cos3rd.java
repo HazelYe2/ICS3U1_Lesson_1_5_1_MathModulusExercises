1df04b3513f3b275efd7963cdff30e955abcdd06
HazelYe2 <Hazel.Ye25@ycdsbk12.ca>
2024-02-26 20:25:31 -0500

    Cos3rd.java

--- Cos3rd.java
+++ Cos3rd.java
@@ -1,13 +1,28 @@
 class Cos3rd extends ConsoleProgram {
 
   /**
-  * Description
-  * @author:
+  * Enter two sides of a triangle and the contained angle, and use cosine law to calculate the third side
+  * @author: H. Ye
   */
   
   public void run() {
     
-    // start coding here
+    // Assign variables
+    double dblSideA;
+    double dblSideB;
+    double dblAngle;
+    double dblSideC;
+
+    // Assign values
+    dblSideA = readDouble ("Enter the length of side A: ");
+    dblSideB = readDouble ("Enter the length of side B: ");
+    dblAngle = readDouble ("Enter the contained angle: ");
+
+    // Calculate using cosine law
+    dblSideC = Math.sqrt (Math.pow (dblSideA, 2) + (Math.pow (dblSideB, 2)) - 2 * dblSideA * dblSideB * Math.cos (Math.toRadians(dblAngle)));
+
+    // Print out the third side
+    System.out.println ("The length of side C is: " + dblSideC);
     
   }
 }
