77886bed224e77a2c7972b2597ff72ed4c77f1cb
HazelYe2 <Hazel.Ye25@ycdsbk12.ca>
2024-02-26 22:03:02 -0500

    Hours.java

--- Hours.java
+++ Hours.java
@@ -1,13 +1,26 @@
 class Hours extends ConsoleProgram {
 
   /**
-  * Description
-  * @author:
+  * Convert hours to days and hours
+  * @author: H. Ye
   */
   
   public void run() {
     
-    // start coding here
+    // Assign variables
+    int intDays;
+    int intHours;
+    int intHours2;
+
+    // Input values
+    intHours = readInt ("Enter the number of hours: ");
+
+    // Calculate hours to days and hours
+    intDays = intHours/24;
+    intHours2 = intHours % 24;
+
+    // Print out the conversion
+    System.out.println ("The result of the conversion is: ");
     
   }
 }
